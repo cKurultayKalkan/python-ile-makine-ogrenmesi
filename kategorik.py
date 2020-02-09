94a66dec5e3aaa2e056800c54fcc6686e84ef2fa
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-02-09 13:17:11 +0300

    kategorik

---
+++ kategorik.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+veriler = pd.read_csv('veriler.csv')
+ulke = veriler.iloc[:, 0:1].values
+
+le = LabelEncoder()
+
+ulke[:, 0] = le.fit_transform(ulke[:, 0])
+
+ct = ColumnTransformer([("all", OneHotEncoder(), [0])], remainder='passthrough')
+X = ct.fit_transform(ulke)
