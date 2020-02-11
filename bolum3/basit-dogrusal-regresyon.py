e1532803c12094558f3eb547e344aff0df9a1bd9
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-02-11 16:05:22 +0300

    linear regresyon done

--- bolum3/basit-dogrusal-regresyon.py
+++ bolum3/basit-dogrusal-regresyon.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
@@ -19,4 +20,17 @@ Y_test = sc.fit_transform(y_test)
 
 # model inşası (linear regression)
 lr = LinearRegression()
-lr.fit(X_train, Y_train)
+lr.fit(x_train, y_train)
+
+tahmin = lr.predict(x_test)
+
+x_train = x_train.sort_index()
+y_train = y_train.sort_index()
+
+plt.plot(x_train, y_train)
+plt.plot(x_test, lr.predict(x_test))
+plt.title("Aylara Göre Satış")
+plt.xlabel("Aylar")
+plt.ylabel("Satışlar")
+
+plt.show()
