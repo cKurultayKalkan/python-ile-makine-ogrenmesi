63125cb61fab2ae3afa5789b562aa95ff45472c0
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-02-11 14:19:32 +0300

    linear regressions class and fit

--- bolum3/basit-dogrusal-regresyon.py
+++ bolum3/basit-dogrusal-regresyon.py
@@ -1,14 +1,22 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
 veriler = pd.read_csv('satislar.csv')
 
 aylar = veriler[['Aylar']]
 satislar = veriler[['Satislar']]
 
+# verilerin ölçeklenmesi
 x_train, x_test, y_train, y_test = train_test_split(aylar, satislar, test_size=0.33, random_state=0)
 
 sc = StandardScaler()
 X_train = sc.fit_transform(x_train)
 X_test = sc.fit_transform(x_test)
+Y_train = sc.fit_transform(y_train)
+Y_test = sc.fit_transform(y_test)
+
+# model inşası (linear regression)
+lr = LinearRegression()
+lr.fit(X_train, Y_train)
